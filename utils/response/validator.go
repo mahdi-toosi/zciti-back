f12ff07d45e483c69f6d6cccb8dd90ef9be15bb4
mahdi toosi <pugnacious0571@gmail.com>
2024-04-19 23:48:47 +0330

    feat: add custom file validator

--- utils/response/validator.go
+++ utils/response/validator.go
@@ -1,7 +1,11 @@
 package response
 
 import (
+	"mime/multipart"
+	"path/filepath"
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/go-playground/locales/fa_IR"
 	ut "github.com/go-playground/universal-translator"
@@ -19,6 +23,11 @@ var (
 
 func init() {
 	validate = validator.New()
+
+	err := validate.RegisterValidation("validate_file", validateFile)
+	if err != nil {
+		return
+	}
 
 	uni = ut.New(fa_IR.New())
 	trans, _ = uni.GetTranslator("fa")
@@ -45,14 +54,54 @@ func ParseAndValidate(c *fiber.Ctx, body any) error {
 
 	switch v.Kind() {
 	case reflect.Ptr:
-		ParseBody(c, body)
+		if err := ParseBody(c, body); err != nil {
+			return err
+		}
 
 		return ValidateStruct(v.Elem().Interface())
 	case reflect.Struct:
-		ParseBody(c, &body)
+		if err := ParseBody(c, &body); err != nil {
+			return err
+		}
 
 		return ValidateStruct(v)
 	default:
 		return nil
 	}
+}
+
+func validateFile(fl validator.FieldLevel) bool {
+	file, ok := fl.Field().Interface().(multipart.FileHeader)
+	if !ok {
+		return false
+	}
+
+	// Get the extension-size pairs from the validation tag
+	pairs := strings.Split(fl.Param(), " ")
+
+	// Check if the file extension is allowed and the file size is within the specified limit
+	fileExtension := filepath.Ext(file.Filename)
+
+	for _, pair := range pairs {
+		parts := strings.Split(pair, ":")
+		ext := strings.TrimSpace(parts[0])
+		sizeStr := strings.TrimSpace(parts[1])
+
+		if ext != fileExtension {
+			continue
+		}
+
+		size, err := strconv.Atoi(sizeStr)
+		if err != nil {
+			// Return false if the size parameter is not a valid integer
+			return false
+		}
+		maxSizeInBytes := int64(size * 1024 * 1024) // Assuming the size parameter is in MB
+
+		// Check if the file size is less than or equal to the maxSize
+		return file.Size <= maxSizeInBytes
+	}
+
+	// If the file extension is not found in the allowed list, return false
+	return false
 }
