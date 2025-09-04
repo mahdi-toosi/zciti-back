2ade543393245af77e8d26f5d0790f968f450263
mahdi toosi <pugnacious0571@gmail.com>
2025-09-04 07:01:31 +0330

    fix(auth): Recaptcha validation in send otp method

--- app/module/auth/request/index.go
+++ app/module/auth/request/index.go
@@ -13,7 +13,7 @@ type Register struct {
 
 type SendOtp struct {
 	Mobile    uint64 `example:"9380338494" validate:"required,number"`
-	Recaptcha string `example:"iasubdi" validate:"required,min=500"`
+	Recaptcha string `example:"iasubdi" validate:"required,min=50"`
 }
 
 type ResetPass struct {
